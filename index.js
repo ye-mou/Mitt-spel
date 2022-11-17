c51075d9766a02fa346533d12bb8c74d68c9c7bd
ye-mou <git.ymo22@gmail.com>
2022-11-17 19:08:16 +0100

    added part of funtions

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
+window.addEventListener.('DOMContentLoaded',function)
+
 const textElement = document.getElementById("welcome");
-const KnappElement = document.getElementById("knappVal");
+const knappElement = document.getElementById("knappVal");
+
+let state = {}
+
+function startGame(){
+    state = {}
+    showTextNode(1)
+
+}
+
+function showTextNode(TesxtNodeIndex){
+
+}
+
+function SelectOption (option){
+
+}
+
+const textNodes = [
+    {
+        id:1
+        text: 'Du måste se att detta inte ör något som vi har koka'
+        
+    }
+]
+
+startGame();
